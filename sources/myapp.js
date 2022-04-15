dfd558870de9de7faa6218d6390d26eef2d775c5
olistopad <olistopad@xbsoftware.com>
2022-04-15 15:08:36 +0300

    [dev] add xbsltd/eslint.git#v4.0.0

--- sources/myapp.js
+++ sources/myapp.js
@@ -1,20 +1,21 @@
+/* eslint-disable no-undef */
+import {JetApp, EmptyRouter, HashRouter} from "webix-jet";
 import "./styles/app.css";
-import {JetApp, EmptyRouter, HashRouter } from "webix-jet";
 
-export default class MyApp extends JetApp{
-	constructor(config){
+export default class MyApp extends JetApp {
+	constructor(config) {
 		const defaults = {
-			id 		: APPNAME,
-			version : VERSION,
-			router 	: BUILD_AS_MODULE ? EmptyRouter : HashRouter,
-			debug 	: !PRODUCTION,
-			start 	: "/top/start"
+			id: APPNAME,
+			version: VERSION,
+			router: BUILD_AS_MODULE ? EmptyRouter : HashRouter,
+			debug: !PRODUCTION,
+			start: "/top/start"
 		};
 
-		super({ ...defaults, ...config });
+		super({...defaults, ...config});
 	}
 }
 
-if (!BUILD_AS_MODULE){
-	webix.ready(() => new MyApp().render() );
+if (!BUILD_AS_MODULE) {
+	webix.ready(() => new MyApp().render());
 }
