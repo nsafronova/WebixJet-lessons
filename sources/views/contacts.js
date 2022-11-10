eea0563e31e42394b47d5aa428ad7834894b0633
nsafronova <natamarykim@gmail.com>
2022-11-10 13:10:32 +0300

    changed structure

--- sources/views/contacts.js
+++ sources/views/contacts.js
@@ -1,7 +1,6 @@
-import {JetView} from "webix-jet";
-
-import ContactsList from "./contacts/contactsList";
+import { JetView } from "webix-jet";
 import Form from "./contacts/form";
+import contacts from "../models/contacts";
 
 
 export default class Contacts extends JetView {
@@ -11,10 +10,51 @@ export default class Contacts extends JetView {
 			paddingX: 5,
 			css: "app_layout",
 			cols: [
-				ContactsList,
+				{
+					view: "list",
+					localId: "mylist",
+					select: true,
+					template: "<span class=\"removeBtn webix_icon wxi wxi-close\"></span> <strong> Name: </strong> #Name# <strong> Email: </strong> #Email# <strong> Status: </strong> #Status# <strong> Country: </strong> #Country# ",
+					css: "webix_shadow_medium",
+					onClick: {
+						removeBtn(e, id) {
+							contacts.remove(id);
+							return false;
+						}
+					}
+				},
 				Form
 			]
 		};
 		return ui;
+	}
+	init() {
+		this.list = this.$$("mylist");
+		this.list.parse(contacts);
+
+		this.on(this.list, "onAfterSelect", (id) => {
+			this.show(`../contacts?id=${id}`);
+		});
+	}
+
+	ready() {
+		this.on(this.app, "onDataEditStop", (contactsData) => {
+			if (contactsData) {
+				if (contactsData.id) contacts.updateItem(contactsData.id, contactsData);
+				else contacts.add(contactsData);
+			}
+		});
+	}
+
+	urlChange() {
+		let id = this.getParam("id");
+		let firstItem = contacts.getFirstId();
+
+		if (id) {
+			this.list.select(id);
+		}
+		else if (firstItem) {
+			this.list.select(firstItem);
+		}
 	}
 }
